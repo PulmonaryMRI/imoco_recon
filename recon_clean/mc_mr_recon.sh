81e71ef01720a9ec072374224fd0cb047baa1bee
Phillistan16 <phillistan705@gmail.com>
2021-04-29 16:28:32 -0700

    commented out soft gating scripts

--- recon_clean/mc_mr_recon.sh
+++ recon_clean/mc_mr_recon.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+*#! /bin/bash
 if [ $# -lt 2 ]
 then
     echo "Not enough arguments supplied"
@@ -12,9 +12,9 @@ sg=$3
 export OMP_NUM_THREADS=32
 
 bart pics -C 20 -i 80 -R T:7:0:$lambda -p $in"_dcf2m" -t $in"_trajm" $in"_datam" $in"_mapsL" $in"_mrL"
-if [$sg -ne 1]
-then
-    bart pics -m -C 20 -i 80 -u 1 -R T:7:0:$lambda -p $in"_dcf2_sg" -t $in"_traj" $in"_data" $in"_maps" $in"_sg"
-else
-    bash mc_sg_nufft.sh $in
-fi
+#if [$sg -ne 1]
+#then
+#    bart pics -m -C 20 -i 80 -u 1 -R T:7:0:$lambda -p $in"_dcf2_sg" -t $in"_traj" $in"_data" $in"_maps" $in"_sg"
+#else
+#    bash mc_sg_nufft.sh $in
+#fi
